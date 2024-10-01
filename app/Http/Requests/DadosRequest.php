3aa746b391a8e54bc433e5717a2d70a8f521e00f
Kawan Paiva <kawanpaiva83@gmail.com>
2024-10-01 18:19:08 -0300

    atualização

--- app/Http/Requests/DadosRequest.php
+++ app/Http/Requests/DadosRequest.php
@@ -6,23 +6,40 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class DadosRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
+    
+    public function authorize()
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
+    public function rules()
     {
         return [
-            //
+        'nome' => 'required|string',
+        'email' => 'required|email',
+        'telefone' => 'required|string',
+        'data_nascimento' => 'required|date',
+        'endereco' => 'required|string',
+        'idade' => 'required|integer',
+        'altura' => 'required|numeric',
         ];
+    }
+    
+    public function messages()
+    {
+        return [
+        'nome.required' => 'O nome é obrigatório.',
+        'email.required' => 'O e-mail é obrigatório.',
+        'telefone.required' => 'O telefone é obrigatório.',
+        'data_nascimento.required' => 'A data de nascimento é obrigatória.',
+        'endereco.required' => 'O endereço é obrigatório.',
+        'idade.required' => 'A idade é obrigatória.',
+        'altura.required' => 'A altura é obrigatória.',
+        'email.email' => 'O e-mail não é válido.',
+        'data_nascimento.date' => 'A data de nascimento não é válida.',
+        'idade.integer' => 'A idade deve ser um número inteiro.',
+        'altura.numeric' => 'A altura deve ser um número.'
+        ];
+        
     }
 }
